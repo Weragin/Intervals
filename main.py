5e8490fd5bad7660f7513567e07e3b250f212369
Weragin <109145593+Weragin@users.noreply.github.com>
2024-02-12 12:04:01 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from typing import List, Set, Tuple
+
+
+def create_intervals(numbers: Set[int]) -> List[Tuple[int, int]]:
+    """
+    Create unit intervals from a set of numbers.
+    """
+    intervals = []
+    start = None
+    last = None
+    for number in sorted(numbers):
+        if not start:
+            start = number
+            last = number
+        elif number == last + 1:
+            last = number
+        else:
+            intervals.append((start, last))
+            start = number
+            last = number
+    intervals.append((start, last))
+    
+    return intervals
+
+def test(data, output):
+    assert create_intervals(data) == output
+
+print(create_intervals({1, 2, 3, 4, 5, 6, 7, 8, 9, 10}))
+print(create_intervals({1, 2, 3, 4, 6, 7, 8, 9}))
+print(create_intervals({1, 6, 9, 10, 7, 8}))
+print(create_intervals({1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 15, 16, 20, 22, 23}))
